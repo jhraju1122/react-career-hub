3de00dcb057cbfa67834d58fc38cb49092dbe937
jhraju <jhraju890@gmail.com>
2024-04-30 09:35:19 +0600

    set nested route and navbar for career hub

---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+// 1// Create brouserrouter setup 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './components/Root/Root.jsx';
+import Home from './components/Home/Home.jsx';
+import AppliedJobs from './components/AppliedJobs/AppliedJobs.jsx';
+import ErrorPage from './components/ErrorPage/ErrorPage.jsx';
+
+
+// 2// Router setup 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/applied',
+        element:<AppliedJobs></AppliedJobs>
+      },
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   {/* 3//RouterProvider setup  */}
+   <RouterProvider router={router} />
+
+  </React.StrictMode>,
+)
